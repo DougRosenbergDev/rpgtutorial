1b173f10603883cf656ebd6953219651d652571f
shalant <doug.rosenberg@gmail.com>
2022-08-27 21:07:52 -0500

    initial commit

---
+++ EFCoreRelationshipsTutorial/Controllers/CharacterController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreRelationshipsTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var characters = await _context.Characters
+                .Where(c => c.UserId == userId)
+                .Include(c => c.Weapon)
+                .ToListAsync();
+
+            return characters;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(CreateCharacterDto request)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            if(user == null)
+                return NotFound();
+
+            var newCharacter = new Character
+            {
+                Name = request.Name,
+                RpgClass = request.RpgClass,
+                User = user
+            };
+
+            _context.Characters.Add(newCharacter);
+            await _context.SaveChangesAsync();
+
+            return await Get(newCharacter.UserId);
+        }
+
+        [HttpPost("weapon")]
+        
+        public async Task<ActionResult<Character>> AddWeapon(AddWeaponDto request)
+        {
+            var character = await _context.Characters.FindAsync(request.CharacterId);
+            if (character == null)
+                return NotFound();
+
+            var newWeapon = new Weapon
+            {
+                Name = request.Name,
+                Damage = request.Damage,
+                Character = character
+            };
+
+            _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+    }
+}
---
+++ EFCoreRelationshipsTutorial/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationshipsTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //entities as sets
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Character> Characters { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
+    }
+}
